968d4625a4f9ece31523694f83132e725de8c58a
Nathan Lee <lee1928@outlook.com>
2024-11-19 15:04:46 +0800

    [1] 20241119 - 2

--- apps/main/rollup.config.dev.js
+++ apps/main/rollup.config.dev.js
@@ -3,15 +3,16 @@ import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import { defineConfig } from 'rollup';
+// import copy from 'rollup-plugin-copy';
 
 export default defineConfig({
     input: 'src/development.ts', // 入口文件
+    external: ['mongoose', 'mongodb'],
     output: {
         dir: 'dist/development', // 输出目录
         format: 'esm', // 输出格式（ES Module）
         sourcemap: true // 生成源码映射文件
     },
-    external: ['mongoose', 'mongodb'],
     plugins: [
         // 将 CommonJS 模块转换为 ES Module
         commonjs(),
@@ -20,6 +21,10 @@ export default defineConfig({
         // 指定 TypeScript 配置文件的位置
         typescript({ tsconfig: './tsconfig.dev.json' }),
         // 告诉 Rollup 要解析的文件扩展名
-        nodeResolve({ extensions: ['.ts', '.tsx', '.js', '.json'] })
+        nodeResolve({ extensions: ['.ts', '.tsx', '.js', '.json'] }),
+        // 复制静态资源文件
+        // copy({
+        //     targets: [{ src: 'src/public', dest: 'dist/development' }]
+        // })
     ]
 });
