8a15f1201689c6249e3391c04ccc60997bc8352f
Nathan Lee <67311306+Nathan3303@users.noreply.github.com>
2024-11-23 12:58:16 +0800

    [1] 更新 Jenkins 脚本文件

--- jenkins.build.sh
+++ jenkins.build.sh
@@ -1,3 +1,6 @@
+# stop docker container mynode
+docker stop mynode
+
 # install pnpm
 npm install -g pnpm --registry=https://registry.npmmirror.com
 
@@ -5,13 +8,31 @@ npm install -g pnpm --registry=https://registry.npmmirror.com
 pnpm install --registry=https://registry.npmmirror.com
 
 # build dist files
-pnpm websrv build
+pnpm run websrv build
 
-# copy certs/ into dist/
-cp -r apps/web/certs apps/web/dist
+# copy dist files into directory bundle
+cp -r apps/web/dist/* apps/web/bundle/
 
 # remove original dist files
-rm -rf /opt/shares/naotodoserver_t
+rm -rf /opt/shares/naotodoserver
 
 # copy dist files to folder /opt/shares(jenkins_share)
-cp -r apps/web/dist /opt/shares/naotodoserver_t
+cp -r apps/web/bundle /opt/shares/naotodoserver
+
+# start docker container mynode
+docker start mynode
+
+# bash into docker container mynode
+docker exec -i mynode /bin/bash -c "pm2 status && cd /opt/shares/naotodoserver && npm install --registry=https://registry.npmmirror.com && pm2 start index.js"
+
+# check pm2 status (optional)
+#pm2 status
+
+# move to folder /opt/shares/naotodoserver_t
+#cd /opt/shares/naotodoserver_t || exit
+
+# list files (optional)
+#ls -l
+
+# exit docker container mynode
+#exit
