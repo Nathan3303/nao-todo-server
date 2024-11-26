31bcccf8277fce231bbb0b60a50154657376bf48
Nathan Lee <67311306+Nathan3303@users.noreply.github.com>
2024-11-26 12:01:52 +0800

    [feat] 新增用户密码修改的路由和接口

--- packages/apis/src/user/update.ts
+++ packages/apis/src/user/update.ts
@@ -1,3 +1,4 @@
+import md5 from 'md5';
 import { User } from '@nao-todo-server/models';
 import {
     getJWTPayload,
@@ -43,4 +44,39 @@ const updateUserNickname = async (req: Request, res: Response) => {
     }
 };
 
-export { updateUserNickname };
+// 更新密码
+const updateUserPassword = async (req: Request, res: Response) => {
+    try {
+        const userId = getJWTPayload(req.headers.authorization as string)
+            .userId as string;
+
+        // 判断请求参数是否合法
+        if (!userId || !req.body.password) {
+            res.json(useErrorResponseData('参数错误，请求无效'));
+            return;
+        }
+
+        // 更新用户密码
+        const updateResult = await User.findOneAndUpdate(
+            { _id: userId },
+            { $set: { password: md5(req.body.password) } },
+            { new: true }
+        );
+
+        // 判断更新结果，返回对应的 JSON 数据
+        if (updateResult) {
+            res.json(
+                useSuccessfulResponseData({
+                    userId: updateResult._id.toString()
+                })
+            );
+        } else {
+            res.json(useErrorResponseData('密码更新失败'));
+        }
+    } catch (error) {
+        console.log('[api/user/updatePassword] Error:', error);
+        res.json(useResponseData(50001, '密码更新失败，请稍后再试', null));
+    }
+};
+
+export { updateUserNickname, updateUserPassword };
