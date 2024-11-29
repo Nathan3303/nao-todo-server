a20e1735c54f2c8dbc0516bdd2e19cf29bcd8ceb
Nathan Lee <67311306+Nathan3303@users.noreply.github.com>
2024-11-29 15:32:52 +0800

    [patch] 优化删除待办接口的逻辑流程

--- packages/apis/src/todo/delete.ts
+++ packages/apis/src/todo/delete.ts
@@ -1,31 +1,52 @@
-import { Todo } from '@nao-todo-server/models';
+import { Comment, Event, Todo } from '@nao-todo-server/models';
 import {
     getJWTPayload,
     useErrorResponseData,
-    useResponseData
+    useResponseData,
+    useSuccessfulResponseData
 } from '@nao-todo-server/hooks';
 import { ObjectId } from '@nao-todo-server/utils';
 import type { Request, Response } from 'express';
 
 const deleteTodo = async (req: Request, res: Response) => {
     try {
+        // 获取请求头中的 UserId
         const userId = getJWTPayload(req.headers.authorization as string)
             .userId as string;
 
-        if (!userId || !req.query.todoId) throw new Error('缺少参数，请求无效');
-
-        const todoId = req.query.todoId as string;
-
-        const deletedTodo = await Todo.findOneAndDelete({
-            _id: new ObjectId(todoId)
-        }).exec();
-
-        if (!deletedTodo) {
-            throw new Error('删除失败，服务器错误');
+        // 判断请求参数是否正确
+        if (!userId || !req.query.todoId) {
+            res.json(useSuccessfulResponseData('缺少参数，请求无效'));
+            return;
         }
 
+        // 删除待办任务
+        const deletedTodo = await Todo.findOneAndDelete({
+            _id: new ObjectId(req.query.todoId as string),
+            userId: new ObjectId(userId)
+        }).exec();
+
+        // 判断删除结果
+        if (!deletedTodo) {
+            res.json(useSuccessfulResponseData('删除失败'));
+            return;
+        }
+
+        // 删除任务下的检查事项和评论
+        await Event.deleteMany({
+            todoId: new ObjectId(req.query.todoId as string),
+            userId: new ObjectId(userId)
+        });
+        await Comment.deleteMany({
+            todoId: new ObjectId(req.query.todoId as string),
+            userId: new ObjectId(userId)
+        });
+
+        // 返回响应
         return res.json(
-            useResponseData(20000, '删除成功', { todoId: deletedTodo._id.toString() })
+            useResponseData(20000, '删除成功', {
+                todoId: deletedTodo._id.toString()
+            })
         );
     } catch (e: unknown) {
         if (e instanceof Error) {
@@ -36,6 +57,6 @@ const deleteTodo = async (req: Request, res: Response) => {
     }
 };
 
-const deleteTodos = async (req: Request, res: Response) => {};
+const deleteTodos = async () => {};
 
 export { deleteTodo, deleteTodos };
