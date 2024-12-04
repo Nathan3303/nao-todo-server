dc32d3fa0f0c0d56894da1472112e10453f6d7f0
Nathan Lee <lee1928@outlook.com>
2024-12-04 20:37:30 +0800

    [patch] 增加删除标签时更新对应待办属性的逻辑

--- packages/apis/src/tag/delete.ts
+++ packages/apis/src/tag/delete.ts
@@ -1,42 +1,58 @@
-import { Tag } from '@nao-todo-server/models';
+import { Tag, Todo } from '@nao-todo-server/models';
 import { ObjectId } from '@nao-todo-server/utils';
 import {
     getJWTPayload,
     useErrorResponseData,
+    useResponseData,
     useSuccessfulResponseData
 } from '@nao-todo-server/hooks';
 import type { Request, Response } from 'express';
 
 const deleteTag = async (req: Request, res: Response) => {
     try {
+        // 获取请求头中的用户 ID
         const userId = getJWTPayload(req.headers.authorization as string)
             .userId as string;
 
+        // 判断请求参数是否正确
         if (!userId || !req.query.tagId) {
-            throw new Error('参数错误，请求无效');
+            res.json(useErrorResponseData('参数错误，请求无效'));
         }
 
-        const tagId = req.query.tagId as string;
-
-        const deletedTag = await Tag.findOneAndDelete({
-            _id: new ObjectId(tagId),
-            userId
+        // 删除标签
+        const deleteResult = await Tag.deleteOne({
+            _id: new ObjectId(req.query.tagId as string),
+            userId: new ObjectId(userId)
         });
 
-        if (!deletedTag) throw new Error('删除失败');
+        // 判断删除结果
+        if (!deleteResult.deletedCount) {
+            res.json(useErrorResponseData('删除失败'));
+        }
 
-        return res.json(
-            useSuccessfulResponseData({ tagId: deletedTag._id.toString() })
+        // 对所有待办移除标签记录
+        const updateResult = await Todo.updateMany(
+            {
+                tags: { $in: [new ObjectId(req.query.tagId as string)] }
+            },
+            {
+                $pull: { tags: new ObjectId(req.query.tagId as string) }
+            }
+        );
+
+        // 响应
+        res.json(
+            useSuccessfulResponseData({
+                tagId: req.query.tagId,
+                effectedTodosCount: updateResult.modifiedCount
+            })
         );
     } catch (e: unknown) {
-        if (e instanceof Error) {
-            return res.json(useErrorResponseData(e.message));
-        }
         console.log('[api/tag/deleteTag] Error:', e);
-        return res.json(useErrorResponseData('服务器错误'));
+        res.json(useResponseData(50001, '删除失败，请稍后再试', null));
     }
 };
 
-const deleteTags = async (req: Request, res: Response) => {};
+const deleteTags = async () => {};
 
 export { deleteTag, deleteTags };
