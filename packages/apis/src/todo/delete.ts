8c37de66da38ecf0ee16205936186d6e23bece0f
Nathan Lee <lee1928@outlook.com>
2024-12-22 22:26:07 +0800

    [feat] 新增批量删除待办的接口：deleteTodos

--- packages/apis/src/todo/delete.ts
+++ packages/apis/src/todo/delete.ts
@@ -57,6 +57,60 @@ const deleteTodo = async (req: Request, res: Response) => {
     }
 };
 
-const deleteTodos = async () => {};
+const deleteTodos = async (req: Request, res: Response) => {
+    try {
+        // 获取请求头中的 UserId
+        const userId = getJWTPayload(req.headers.authorization as string)
+            .userId as string;
+
+        // 判断请求参数是否正确
+        if (!userId || !req.body.todoIds) {
+            res.json(useSuccessfulResponseData('缺少参数，请求无效'));
+            return;
+        }
+
+        // 删除待办任务
+        const deleteResult = await Todo.deleteMany({
+            _id: {
+                $in: req.body.todoIds.map((id: string) => new ObjectId(id))
+            },
+            userId: new ObjectId(userId)
+        }).exec();
+
+        // 判断删除结果
+        if (!deleteResult.deletedCount) {
+            res.json(useSuccessfulResponseData('删除失败'));
+            return;
+        }
+
+        // 删除任务下的检查事项和评论
+        await Event.deleteMany({
+            todoId: {
+                $in: req.body.todoIds.map((id: string) => new ObjectId(id))
+            },
+            userId: new ObjectId(userId)
+        });
+        await Comment.deleteMany({
+            todoId: {
+                $in: req.body.todoIds.map((id: string) => new ObjectId(id))
+            },
+            userId: new ObjectId(userId)
+        });
+
+        // 返回响应
+        return res.json(
+            useResponseData(20000, '删除成功', {
+                todoIds: req.body.todoIds,
+                count: deleteResult.deletedCount
+            })
+        );
+    } catch (e) {
+        if (e instanceof Error) {
+            return res.json(useErrorResponseData(e.message));
+        }
+        console.log('[api/todo/deleteTodos] Error:', e);
+        return res.json(useErrorResponseData('服务器错误'));
+    }
+};
 
 export { deleteTodo, deleteTodos };
