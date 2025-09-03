774cae32708d1a4c8a69745fea4f8c12995f4b32
Natan3303 <lee1928@outlook.com>
2025-09-03 21:49:23 +0800

    arch(clear): 删除所有旧架构文件

--- packages/apis/src/auth/index.ts
+++
@@ -1,7 +0,0 @@
-import checkin from './checkin';
-import signin from './signin';
-import signup from './signup';
-import signout from './signout';
-import validate from './validate';
-
-export { checkin, signin, signup, signout, validate };
