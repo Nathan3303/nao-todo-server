b9e3d21cd254f1762a10e2f12589b943a307b479
Natan3303 <lee1928@outlook.com>
2025-09-15 13:50:49 +0800

    feat(todo): 实现了基础的待办 API

--- models/todo.go
+++ models/todo.go
@@ -1,1 +1,22 @@
 package models
+
+import "time"
+
+type Todo struct {
+	Model
+
+	UserId       int64  `gorm:"not null" json:"userId"`
+	ProjectId    int64  `gorm:"not null" json:"projectId"`
+	ParentTodoId *int64 `gorm:"null" json:"parentTodoId"`
+
+	Name        string     `gorm:"size:64" json:"name"`
+	Description string     `gorm:"size:256" json:"description"`
+	State       int8       `json:"state"`
+	Priority    int8       `json:"priority"`
+	StartAt     *time.Time `gorm:"null" json:"startAt"`
+	EndAt       *time.Time `gorm:"null" json:"endAt"`
+	ArchivedAt  *time.Time `gorm:"null" json:"archivedAt"`
+	FavoritedAt *time.Time `json:"favoritedAt"`
+	GivenUpAt   *time.Time `json:"givenUpAt"`
+	Tags        []int64    `gorm:"serializer:json;type:json" json:"tags"`
+}
---
+++ routers/todoRouter.go
@@ -0,0 +1,25 @@
+package routers
+
+import (
+	"naotodoserver/apis"
+	todoapis "naotodoserver/apis/todo"
+	userapis "naotodoserver/apis/user"
+
+	"github.com/gin-gonic/gin"
+)
+
+func TodoRouterInit(router *gin.RouterGroup) {
+	todoRouter := router.Group("/todo", userapis.ValidateHandlerV1)
+	{
+		todoRouter.GET("/:todoId", todoapis.GetTodoHandlerV1)
+		todoRouter.POST("/", todoapis.CreateTodoHandlerV1)
+		todoRouter.PUT("/:todoId", todoapis.UpdateTodoHandlerV1)
+		todoRouter.DELETE("/:todoId", todoapis.DeleteTodoHandlerV1)
+		todoRouter.PUT("/restore/:todoId", apis.DefaultHandler)
+	}
+	todosRouter := router.Group("/todos", userapis.ValidateHandlerV1)
+	{
+		todosRouter.GET("/", todoapis.GetTodosHandlerV1)
+		todosRouter.PUT("/", apis.DefaultHandler)
+	}
+}
