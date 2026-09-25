5205362dfec602278689ec39929e3d3e4217b3f1
Nathan3303 <Lee1928@outlook.com>
2026-09-25 15:16:32 +0800

    test(T206): 【演练还原】删除演练探针文件
    还原 run#2 的故意违规；squash 后本提交与上一条演练提交净效果为 0（最终 diff 无该文件）。

--- domain/textutils/zz_lint_drill_tmp.go
+++
@@ -1,9 +0,0 @@
-// T206 全量阻断失败演练：临时探针文件，下一条提交即删除，不进入最终 diff。
-package textutils
-
-import "encoding/json"
-
-func zzLintDrillTmp(s string) string {
-	json.Marshal(s)
-	return s + "0" + "1" + "2" + "3" + "4" + "5" + "6" + "7" + "8" + "9" + "a" + "b" + "c" + "d" + "e" + "f" + "g" + "h" + "i" + "j" + "k" + "l" + "m" + "n" + "o" + "p"
-}
